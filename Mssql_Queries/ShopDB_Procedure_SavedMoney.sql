44d4a9bd96fada2321a6ca31072ab880a6554912
mkrzyszton <80623680+mkrzyszton@users.noreply.github.com>
2022-01-16 14:25:50 +0100

    Update ShopDB_Procedure_SavedMoney.sql

--- Mssql_Queries/ShopDB_Procedure_SavedMoney.sql
+++ Mssql_Queries/ShopDB_Procedure_SavedMoney.sql
@@ -7,9 +7,9 @@ as
 begin
 				--cast((ProductPrice * (Discount * 0.01) - (ProductPrice * (Discount * 0.01)*(Margin * 0.01)))
 	select distinct *, cast((ProductPrice * (Discount * 0.01)) as numeric(10,2)) as SavedMoney from Product 
-		inner join OrderedProduct on Product.ProductID = OrderedProduct.ProductID
-		inner join Company on Product.CompanyID = Company.CompanyID
-		inner join Category on Product.CategoryID = Category.CategoryID
+		join OrderedProduct on Product.ProductID = OrderedProduct.ProductID
+		join Company on Product.CompanyID = Company.CompanyID
+		join Category on Product.CategoryID = Category.CategoryID
 	where CategoryName = @CategoryName
 	order by SavedMoney desc;
 end
